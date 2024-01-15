a07617e01224226275e79dbabd0facea60884077
Mario Caricasole <caricasolemario@gmail.com>
2024-01-15 13:42:37 +0100

    added Amplitude and NSModel

--- PINN.py
+++ PINN.py
@@ -1,7 +1,7 @@
 # Import necessary libraries
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Reshape
 import keras
 import tensorflow as tf
 import copy
@@ -17,38 +17,64 @@ class PeriodicWeights(keras.initializers.Initializer):
         tensor = tf.reshape(kVec, [shape[0], shape[1]])
         return tensor
 
-#define a periodic layer using the cosine as activation and a fixed phase, defined with trainable parameters
+#define a periodic layer using the cosine or sine as activation, using non-trainable parameters
 class Periodic(keras.layers.Layer):
-    def __init__(self, freqs=16, input_dim=4):
+    def __init__(self, activation=tf.cos, freqs=16, input_dim=3):
         super().__init__()
         self.w = self.add_weight(
             shape=(input_dim,freqs**3), initializer=PeriodicWeights, trainable=False)
-        self.b = self.add_weight(
-            shape=(freqs**3,), initializer="zeros", trainable=True)
+        self.activation = activation
 
     def call(self,inputs):
-        return tf.cos(2*np.pi*tf.matmul(inputs,self.w) + self.b)
+        return self.activation(2*np.pi*tf.matmul(inputs,self.w))
 
-#define fixed amplitudes model
-model = Sequential(
-    [
-        Periodic(20,3),
-        Dense(1)
-    ])
+#define time-dependent amplitudes model
+class AmplitudeModel(keras.Model):
+    def __init__(self, freqs = 16, output_size = 4):
+        super().__init__()
+        self.freqs = freqs
+        self.output_size = output_size
 
-model.build((None,3))
-model.compile(optimizer='adamax', loss='mse')
+        #define the layers of the model
+        self.dense_layer = Dense(output_size*freqs**3, activation='sigmoid')
+        self.output_layer = Dense(output_size*freqs**3)
+        self.reshape_layer = Reshape((output_size,freqs**3))
 
-#define initial condition
-def f(xVec):
-    x = xVec[:,0]
-    y = xVec[:,1]
-    z = xVec[:,2]
+    def call(self,inputs):
+        layer1 = self.dense_layer(inputs)
+        output = self.output_layer(layer1)
+        output = self.reshape_layer(output)
 
-    return tf.cos(2*np.pi*z) + tf.sin(2*np.pi*(3*z + 4*x))
+        return output
 
-#define grid for spatial coordinate
-iterations = 1000
-for i in range(iterations):
-    points = tf.random.uniform([100,3],0.0,1.0)
-    model.fit(points,f(points))
+class NSModel(tf.keras.Model):
+    def __init__(self, freqs=16, output_size=4):
+        super().__init__()
+
+        self.cos_periodic = Periodic(activation=tf.cos, freqs=freqs)
+        self.sin_periodic = Periodic(activation=tf.sin, freqs=freqs)
+
+        self.cos_amplitude = AmplitudeModel(freqs=freqs, output_size=output_size)
+        self.sin_amplitude = AmplitudeModel(freqs=freqs, output_size=output_size)
+
+    def call(self, inputs):
+        # Split the input tensor into the first three variables and the last one
+        periodic_inputs = inputs[:, :3]
+        amplitude_input = inputs[:, 3:]
+
+        # Compute outputs from periodic layers
+        cos_output = self.cos_periodic(periodic_inputs)
+        sin_output = self.sin_periodic(periodic_inputs)
+
+        # Compute outputs from amplitude models
+        cos_amplitude_output = self.cos_amplitude(amplitude_input)
+        sin_amplitude_output = self.sin_amplitude(amplitude_input)
+
+        # Element-wise multiplication and sum over frequencies
+        combined_cos = tf.einsum('ijk,ik -> ij', cos_amplitude_output, cos_output)
+        combined_sin = tf.einsum('ijk,ik -> ij', sin_amplitude_output, sin_output)
+
+        # Sum the results along the last axis
+        final_output = combined_cos + combined_sin
+
+        return final_output
